feabbf481e8f6cb1674e979a9eb0db4f813339d2
João Vitor Albero <contato.joaoalbero@gmail.com>
2025-11-26 16:14:30 -0300

    Updating the .env archive and testing the 2FA / Atualizando o arquivo .env e testando a 2FA

--- src/backend/services/emailService.js
+++ src/backend/services/emailService.js
@@ -1,10 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import nodemailer from "nodemailer";
 
-const host = process.env.SMTP_HOST;
-const port = Number(process.env.SMTP_PORT);
+const host = process.env.SMTP_HOST || "smtp.gmail.com";
+const port = Number(process.env.SMTP_PORT || "587");
 const user = process.env.SMTP_USER;
 const pass = process.env.SMTP_PASS;
 const from = process.env.SMTP_FROM;
+
+console.log("[MAIL] Using SMTP config:", { host, port, user });
 
 const transporter = nodemailer.createTransport({
   host,
