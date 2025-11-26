8ad21069c60fb6d7894aaa20f12172bcc2d2f79a
João Vitor Albero <contato.joaoalbero@gmail.com>
2025-11-26 16:49:33 -0300

    Atualizando authroutes.js / Updating authroutes.js

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { healthAuth, register, verifyEmail } from "../controllers/authController.js";
+import { healthAuth, register, verifyEmail, login, verifyLoginCode } from "../controllers/authController.js";
 
 export const router = Router();
 
 router.get("/health", healthAuth);
 router.post("/register", register);
 router.post("/verify-email", verifyEmail);
+router.post("/login", login);
+router.post("/verify-login", verifyLoginCode);
