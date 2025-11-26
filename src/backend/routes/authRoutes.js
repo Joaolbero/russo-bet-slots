bc8244f3a56bc80b9e92088a568a9043a9885d65
João Vitor Albero <contato.joaoalbero@gmail.com>
2025-11-26 14:35:06 -0300

    Updating the connection archive / Atualizando o arquivo de conexão

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { healthAuth } from "../controllers/authController.js";
+import { healthAuth, register, verifyEmail } from "../controllers/authController.js";
 
 export const router = Router();
 
 router.get("/health", healthAuth);
+router.post("/register", register);
+router.post("/verify-email", verifyEmail);git add .
+git commit -m "Updating the connection archive / Atualizando o arquivo de conexão"
+git push origin main
